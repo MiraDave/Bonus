143ee5e402337264ff007d7da38240cc6476d9e1
MiraDave <ilmiradavletshina25@gmail.com>
2020-09-15 00:50:28 +0500

    Bonus

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-    long currentinvoice = 100;
-    long additionalinvoice = 1100;
+    long currentinvoice = 0;
+    long additionalinvoice = 1000;
     long bonus = additionalinvoice / 100;
-
-    long totalinvoice = currentinvoice + additionalinvoice + bonus;
+    long totalinvoice;
+        if (additionalinvoice >= 1000) {
+            totalinvoice = currentinvoice + additionalinvoice + bonus;
+        } else {
+            totalinvoice = currentinvoice + additionalinvoice;
+    }
     System.out.println(totalinvoice);
     }
 }
